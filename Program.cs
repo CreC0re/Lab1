db2322b9c95d0b2f9fea74fd142472977620b83f
CreC0re <twizyyy28@gmail.com>
2025-03-18 08:32:38 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Adres
+{
+    public string City;
+    public string Streat;
+    public int Numhouse;
+
+    public Adres(string city, string streat, int numhouse)
+    {
+        City = city;
+        Streat = streat;
+        Numhouse = numhouse;
+    }
+
+    public void ShowAdres()
+    {
+        Console.WriteLine($"Адрес: {City}, ул. {Streat}, д. {Numhouse}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Adres adres = new Adres("Москва", "Ленина", 10);
+        adres.ShowAdres();
+
+        Console.ReadLine();
+    }
+}
